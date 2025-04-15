290e5605616c90b15acd97a236cc9310ddb12136
ham1dul-islam <hamidulomith@gmail.com>
2025-04-15 08:48:17 -0400

    Update add_column_and_foreign_key.sql

--- add_column_and_foreign_key.sql
+++ add_column_and_foreign_key.sql
@@ -1,7 +1,7 @@
-ALTER TABLE Contracts
-ADD COLUMN contract_po_or_req INTEGER; -- Assuming the primary key in Contract_PO_or_Req is an integer. Adjust the data type if it's different.
+ALTER TABLE public."Contract"
+ADD COLUMN contract_po_or_req VARCHAR(36); -- Assuming the primary key in Contract_PO_or_Req is an integer. Adjust the data type if it's different.
 
-ALTER TABLE Contracts
+ALTER TABLE public."Contract"
 ADD CONSTRAINT fk_contract_po_or_req
 FOREIGN KEY (contract_po_or_req)
-REFERENCES Contract_PO_or_Req(id); -- Replace 'id' with the actual primary key column name in Contract_PO_or_Req
+REFERENCES public."Contract_PO_or_Req"(id); -- Replace 'id' with the actual primary key column name in Contract_PO_or_Req
